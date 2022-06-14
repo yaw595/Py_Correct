a934a9afce99a635edc349443ffd9921c429c716
yaw595 <39474651+yaw595@users.noreply.github.com>
2022-06-14 15:16:16 +0000

    updated for multiple files

--- correct.py
+++ correct.py
@@ -11,6 +11,7 @@ files = glob.glob(path + '/*.xlsx')  # Specifying the file extension for the fil
 # Loop over all the files and load into a Pandas' data frame
 for file in files:
     temp_df = pd.read_excel(file, sheet_name='Individuals')
+    sheet_2 = pd.read_excel(file, sheet_name='Extract')
     if len(temp_df.index) > 95:
         temp_df = temp_df.drop([95, 96])
     else:
@@ -32,17 +33,19 @@ for file in files:
             suggestion = suggestion + '_' + parts[1]  # Recombining the name and number for the individual
             corrected_list.append(suggestion)  # Adding the corrected names to a corrected list
 
-    temp_df['Temporary_name'] = corrected_list  # Replacing errors in the 'TemporaryName' column with the
-
-    # uncomment the line below to print the list of corrected order names:
+    temp_df['TemporaryName'] = corrected_list  # Replacing errors in the 'TemporaryName' column with the
     # print(len(corrected_list),  ':', corrected_list)
 
     # Remove the ['.file extension'] (line 1) and add '_corrected' to the file name (line 2)
     new_file = file.split('.')
-    new_file_name = (new_file[0] + '_corrected.' + new_file[1]).replace('data', 'corrected')
+    new_file_name = (new_file[0] + '_corrected.' + new_file[1]).replace('data', 'corrected/batch_1')
 
-    # Print out each file name and it's corresponding
+    # Print out each file name, and it's corresponding
     print(f'{new_file_name}: {len(corrected_list)} : {corrected_list}')
-
+    temp_df.rename(columns={'Unnamed: 0': '', 'Unnamed: 1': '', 'Unnamed: 2': ''}, inplace=True)
     # uncomment the line below to save the file with the corrections in the 'corrected' folder:
-    # temp_df.to_excel('./' + new_file_name)
+    path = './' + new_file_name
+    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+    temp_df.to_excel(writer, sheet_name='Individuals', index=False)
+    sheet_2.to_excel(writer, sheet_name='Extract', index=False)
+    writer.save()
